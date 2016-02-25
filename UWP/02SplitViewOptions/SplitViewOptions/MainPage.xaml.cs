4245a59bd14181e94f05e6fbf2966b1af0b5d069
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2016-02-25 20:07:53 +0100

    SplitViewOptions - init

---
+++ UWP/02SplitViewOptions/SplitViewOptions/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SplitViewOptions
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.MySplitView.IsPaneOpen = this.MySplitView.IsPaneOpen ? false : true;
+        }
+
+		private void RadioButtonPaneItem_Click(object sender, RoutedEventArgs e)
+		{
+			var radioButton = sender as RadioButton;
+
+			if (radioButton != null)
+			{
+				switch (radioButton.Tag.ToString())
+				{
+					case "Map":
+						this.MainFrame.Navigate(typeof(MapPage));
+						break;				
+				}
+			}
+		}
+	}
+}
